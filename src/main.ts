5e954fd350737e934f223e1d3bf0990d79ebc699
lao <562553897@qq.com>
2020-09-18 12:04:36 +0800

    feat：中间件使用

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogMiddleware } from './middlewares/log.middleware';
+import { test1MiddleWares } from './middlewares/test';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 使用自定义中间件
+  app.use(test1MiddleWares());
   await app.listen(3000);
 }
 bootstrap();
