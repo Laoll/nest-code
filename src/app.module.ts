4fb9e53b7e78878bc95dc1fbe75a7cae5c9b47d6
lao <562553897@qq.com>
2020-09-18 12:04:06 +0800

    feat: orm 配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,45 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogModule } from './modules/log/log.module';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import { PostModule } from './modules/post/post.module';
+import { TagModule } from './modules/tag/tag.module';
+import { RoleModule } from './modules/role/role.module';
+import * as path from 'path';
+import { LogMiddleware } from './middlewares/log.middleware';
 
 @Module({
-  imports: [UserModule, LogModule],
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}'), {
+      modifyConfigName: name => name.replace('.config', ''),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        type: config.get('database.type'),
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: config.get('database.logging'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    LogModule,
+    PostModule,
+    TagModule,
+    RoleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('*');
+  }
+}
